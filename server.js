8d129f615276f898f6d6e979facaef6e2c939ac7
chihsuan <chihsuan.tw@gmail.com>
2015-11-23 20:52:20 +0800

    update server.js

--- server.js
+++ server.js
@@ -6,15 +6,28 @@ var http = require('http');
 //mongodb
 global.mongo= mongoose.createConnection(CONFIG.mongo);
 var mongo= require(CONFIG.Controllers('mongo'));
+var Pgb = require("pg-bluebird");
+var pgb = new Pgb();
+CONFIG.pg = "postgres://chihsuan@localhost/dmsp";
+pgb.connect(CONFIG.pg)
+  .then(function(connection) {
+    global.pg = connection;
+  })
+  .catch(function(err) {
+    console.log('pg not connect');
+  });
+var pg= require(CONFIG.Controllers('pg'));
 
 
 http.createServer(function(req, res) {
   var arr = req.url.split('/');
   if (arr.length === 5 && arr[3] == 'DataInsertion') {
-    mongo.create(arr[4], res);
+    //mongo.create(arr[4], res);
+    pg.create(arr[4], res);
   }
   else if (arr.length === 4 && arr[3] == 'SelectCurrentData') {
-    mongo.count(req, res);
+    //mongo.count(req, res);
+    pg.count(req, res);
   }
 }).listen(8080, function() {
   console.log('listening on 8080');
